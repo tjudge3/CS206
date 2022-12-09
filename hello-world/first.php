ab0b8e13a2e5888a80ecd43518a207879a35cf15
tjudge3 <67015636+tjudge3@users.noreply.github.com>
2022-12-09 18:39:03 -0500

    initial commit

---
+++ hello-world/first.php
@@ -0,0 +1,25 @@
+//This was a hello world assignment 
+//Also asked was different uses of commenting
+<!DOCTYPE html>
+<html lang = "en">
+<head>
+    <title>First PHP Script</title>
+    <meta charset = "utf-8">
+</head>
+<body>
+<?php
+
+//Comment 1
+echo "<h1>Hello World Wide Web!</h1>";
+#Comment 2
+print "<p>This is my first PHP program.</p>";
+
+/*
+Comment 3
+*/
+echo "It is often said \"The truth is rarely pure\"";
+
+?>
+
+</body>
+</html>
